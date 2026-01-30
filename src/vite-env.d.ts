c4c4d8534fade2f997f35d522e94bc96eb44e81d
钟纪杰 <10463783+zhong-jijie@user.noreply.gitee.com>
2026-01-30 18:28:30 +0800

    修改ts配置

---
+++ src/vite-env.d.ts
@@ -0,0 +1,31 @@
+/// <reference types="vite/client" />
+
+declare module '*.css' {
+  const content: { [className: string]: string }
+  export default content
+}
+
+declare module '*.scss' {
+  const content: { [className: string]: string }
+  export default content
+}
+
+declare module '*.sass' {
+  const content: { [className: string]: string }
+  export default content
+}
+
+declare module '*.svg' {
+  const content: React.FC<React.SVGProps<SVGSVGElement>>
+  export default content
+}
+
+declare module '*.png' {
+  const content: string
+  export default content
+}
+
+declare module '*.jpg' {
+  const content: string
+  export default content
+}
